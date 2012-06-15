e316928a273af7536ea83830e4b1b3ae8060f3b4
Samuel Cochran <sj26@sj26.com>
2012-06-15 18:57:10 +0800

    New rails app

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source :rubygems
+
+gem 'rails', '~> 3.2.5'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'yui-compressor'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubygemsOrg::Application.config.secret_token = '7bf43adca827cad0e399305774a20bb60561d6923da14b63f67dfc7c41f26541763ac785e0c4f62c6a6bafe8294878b41e654e4e960509135240f9cfee5bbf42'
