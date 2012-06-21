105a2c1fc1589c8146c48ccd7d02257bccbc226e
Samuel Cochran <sj26@sj26.com>
2012-06-22 06:03:57 +0800

    Add "exact match" and preserve search order

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.order(:name).paginate page: params[:page] || 1
-    @projects = @projects.search params[:search] if search?
+    @projects = Project.scoped
+    if search?
+      @projects = @projects.search params[:search] if search?
+      @project = Project.find_by_name params[:search] if (params[:page] || 1).to_i == 1
+    else
+      @projects = @projects.order(:name)
+    end
+    @projects = @projects.paginate page: params[:page] || 1
   end
 
   def show
