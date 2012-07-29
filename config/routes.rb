e324ebf4282fd4c0c2349d34a62a46358f042f37
Samuel Cochran <sj26@sj26.com>
2012-07-29 22:37:29 +0800

    Projects might have weird IDs too

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rubygems::Application.routes.draw do
   root to: "home#show"
 
-  resources :projects, only: [:show, :index]
+  resources :projects, only: [:show, :index], constraints: {id: /[^\/]+/}
 
   resources :versions, path: :gems, only: [:show], constraints: {id: /[^\/]+/} do
     get :browse, path: "browse(/*path)", on: :member, constraints: {format: false}
